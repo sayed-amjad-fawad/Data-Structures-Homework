76aa81444fa913bcbdbf83cab8b364c984a66fdb
Sayed Amjad Fawad <142443686+sayed-amjad-fawad@users.noreply.github.com>
2025-12-21 23:40:45 +0300

    Refactor linked list to use 'item' and add functions
    Updated linked list implementation to use 'item' instead of 'data'. Added push and deleteItem functions for better clarity and functionality.

--- Week_03/linked_list.c
+++ Week_03/linked_list.c
@@ -1,72 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define the structure of a node
 struct Node {
-    int data;
+    int item;
     struct Node* next;
 };
 
-// 1. Traversal: Print the list
-void printList(struct Node* n) {
-    while (n != NULL) {
-        printf("%d -> ", n->data);
-        n = n->next;
+void printList(struct Node* ptr) {
+    while (ptr != NULL) {
+        printf("%d -> ", ptr->item);
+        ptr = ptr->next;
     }
     printf("NULL\n");
 }
 
-// 2. Insertion at the Front
-void insertFront(struct Node** head_ref, int new_data) {
-    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
-    new_node->data = new_data;
-    new_node->next = (*head_ref);
-    (*head_ref) = new_node;
+void push(struct Node** headPtr, int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->item = val;
+    newNode->next = *headPtr;
+    *headPtr = newNode;
 }
 
-// 3. Insertion at the End
-void insertEnd(struct Node** head_ref, int new_data) {
-    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
-    struct Node* last = *head_ref;
-    new_node->data = new_data;
-    new_node->next = NULL;
-    if (*head_ref == NULL) {
-        *head_ref = new_node;
+void deleteItem(struct Node** headPtr, int key) {
+    struct Node *tmp = *headPtr, *prev = NULL;
+    
+    if (tmp != NULL && tmp->item == key) {
+        *headPtr = tmp->next;
+        free(tmp);
         return;
     }
-    while (last->next != NULL) last = last->next;
-    last->next = new_node;
-}
-
-// 4. Deletion by Value
-void deleteNode(struct Node** head_ref, int key) {
-    struct Node *temp = *head_ref, *prev;
-    if (temp != NULL && temp->data == key) {
-        *head_ref = temp->next;
-        free(temp);
-        return;
+    
+    while (tmp != NULL && tmp->item != key) {
+        prev = tmp;
+        tmp = tmp->next;
     }
-    while (temp != NULL && temp->data != key) {
-        prev = temp;
-        temp = temp->next;
-    }
-    if (temp == NULL) return;
-    prev->next = temp->next;
-    free(temp);
+    
+    if (tmp == NULL) return;
+    
+    prev->next = tmp->next;
+    free(tmp);
 }
 
 int main() {
     struct Node* head = NULL;
 
-    insertEnd(&head, 10);
-    insertFront(&head, 20); // List: 20 -> 10
-    insertEnd(&head, 30);   // List: 20 -> 10 -> 30
+    push(&head, 30);
+    push(&head, 20);
+    push(&head, 10);
     
-    printf("Linked List after insertions: ");
+    printf("Current List: ");
     printList(head);
 
-    deleteNode(&head, 10);
-    printf("Linked List after deleting 10: ");
+    deleteItem(&head, 20);
+    printf("After deleting 20: ");
     printList(head);
 
     return 0;
